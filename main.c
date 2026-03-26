8af112883b4a7163ea21c22ad2fb8e398494601c
karatellll <serikovurij2@gmail.com>
2026-03-26 14:31:43 +0300

    sum

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int sum(int num1, int num2) {
+    return num1 + num2;
+}
+
+int main() {
+    int num1 = 3;
+    int num2 = 4;
+    int result;
+    result = sum(num1, num2);
+    printf("sum = %d\n", result);
+}
+
+

65d415f0632695aa139cefb56cf9abb48b172cbe
karatellll <serikovurij2@gmail.com>
2026-03-26 14:41:20 +0300

    mult

--- main.c
+++ main.c
@@ -4,12 +4,20 @@ int sum(int num1, int num2) {
     return num1 + num2;
 }
 
+int mult(int num1, int num2) {
+    return num1 * num2;
+}
+
 int main() {
     int num1 = 3;
     int num2 = 4;
     int result;
+    
     result = sum(num1, num2);
     printf("sum = %d\n", result);
+
+    result = mult(num1, num2);
+    printf("mult = %d\n", result);
 }
 
 

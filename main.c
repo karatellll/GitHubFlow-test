54b8d5d842024538240cbe552a645fb2899d6eee
karatellll <serikovurij2@gmail.com>
2026-03-26 14:50:01 +0300

    div

--- main.c
+++ main.c
@@ -8,8 +8,12 @@ int mult(int num1, int num2) {
     return num1 * num2;
 }
 
+int div(int num1, int num2) {
+    return num1 / num2;
+}
+
 int main() {
-    int num1 = 3;
+    int num1 = 8;
     int num2 = 4;
     int result;
     
@@ -18,6 +22,9 @@ int main() {
 
     result = mult(num1, num2);
     printf("mult = %d\n", result);
+
+    result = div(num1, num2);
+    printf("div = %d\n", result);
 }
 
 
